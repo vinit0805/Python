854e29e50a95abbc7c9ae107cd25d35691ebd180
Vinit Malik <77112781+vinit0805@users.noreply.github.com>
2021-01-15 21:27:28 +0530

    Add files via upload

---
+++ python 1.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x = 'Hello world'
+print(x)
+
+
+# In[2]:
+
+
+y = 100 
+print(y)
+
+
+# In[3]:
+
+
+age = 25
+Age = 50
+AGE = 75
+print(age,Age,AGE)
+
+
+# In[4]:
+
+
+1age = 25
+
+
+# In[5]:
+
+
+_age = 25 
+print(_age)  # A-Z , a-z,  0-9, _
+
+
+# In[6]:
+
+
+x = 'hello world'
+print(x)
+
+
+# In[7]:
+
+
+print('hello world')
+
+
+# In[8]:
+
+
+x = "HELLO"
+y = "WORLD"
+
+print(x)
+print(y)
+
+
+# In[9]:
+
+
+print(x, end = '_')
+
+
+# In[ ]:
+
+
+
+
+
+# In[21]:
+
+
+fruit1 = 'APPLE'
+fruit2 = 'MANGO'
+
+print("i have an "+fruit1+)
+
+
+# In[25]:
+
+
+a = 54
+
+
+# In[26]:
+
+
+a = 56
+
+
+# In[27]:
+
+
+a = 55
+
+
+# In[29]:
+
+
+print(a)
+
+
+# In[33]:
+
+
+b = input("Entre your name:")
+print("hello" + b)
+
